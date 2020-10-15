f2bee5d98df96df232f2708f41e12f70ccdcec85
David Medina <davidamedina619@gmail.com>
2020-10-15 19:15:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index(1) do |dwarves, index|
+  array.each_with_index do |dwarves, index|
     puts "#{index}. #{dwarves}"  
   end 
   
